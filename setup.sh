60c8bc9635ee4982f08a1365a90bc4705bcbe869
Zac Adams <zac@zacada.net>
2025-01-30 06:41:05 +0000

    Add the rest of brews setup steps

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-sudo apt update
-sudo apt install -y
+echo >> /home/coder/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/coder/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+sudo apt-get update
+sudo apt-get install -y build-essential
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 export KUBECONFIG=~/coder/.kube/config
 export SOPS_AGE_KEY_FILE=~/coder/age.agekey
+brew install gcc
 brew install fluxcd/tap/flux
